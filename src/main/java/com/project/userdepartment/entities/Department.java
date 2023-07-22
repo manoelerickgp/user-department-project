bfa76b0185c8088744596f04b2eb3faa0afdef63
manoelerickgp <manoel.computacao@gmail.com>
2023-07-21 23:59:04 -0300

    h2 database

--- src/main/java/com/project/userdepartment/entities/Department.java
+++ src/main/java/com/project/userdepartment/entities/Department.java
@@ -1,9 +1,15 @@
 package com.project.userdepartment.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_department")
 public class Department {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
