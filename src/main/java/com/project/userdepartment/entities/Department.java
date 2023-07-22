d1d7a9d7ae493b3fb2a6bfe699fc24f230c54b66
manoelerickgp <manoel.computacao@gmail.com>
2023-07-21 23:43:14 -0300

    project initiated

---
+++ src/main/java/com/project/userdepartment/entities/Department.java
@@ -0,0 +1,46 @@
+package com.project.userdepartment.entities;
+
+import java.util.Objects;
+
+public class Department {
+
+    private Long id;
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/test/java/com/project/userdepartment/UserdepartmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.userdepartment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdepartmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
